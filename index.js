5033505e204986dc8eabaaa3f91d00af46b81bb0
Marc Delalonde <marcdelalonde@gmail.com>
2021-09-28 15:14:53 +0200

    General enhancements

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ import commander from 'commander';
 import inquirer from 'inquirer';
 import figlet from 'figlet';
 import ora from 'ora';
+import axios from 'axios';
 
 import * as templateGenerator from './helpers/generator.js';
 import * as mongodbHelper from './helpers/mongodb.js';
 
 figlet('Adminmate', function(err, data) {
+  console.log('');
   console.log(data);
 
   // Launch command line program
   commandLine();
 });
 
-const databaseQuestions = [
-  {
-    name: 'database',
-    type: 'list',
-    message: 'What kind of database you want to use ?',
-    choices: ['mysql', 'postgresql', 'sqlite', 'mongodb']
-  },
-  // {
-  //   name: 'uri',
-  //   type: 'input',
-  //   message: 'What the connection uri of your database ? (ex: mongodb://username:password@host:port/database)'
-  // }
-];
+const databaseQuestion = {
+  name: 'database',
+  type: 'list',
+  message: 'What kind of database you want to use ?',
+  choices: ['mysql', 'postgresql', 'sqlite', 'mongodb']
+};
+// {
+//   name: 'uri',
+//   type: 'input',
+//   message: 'What the connection uri of your database ? (ex: mongodb://username:password@host:port/database)'
+// }
 
 const projectQuestions = [
   {
@@ -37,38 +37,83 @@ const projectQuestions = [
   }
 ];
 
+const checkReqValidity = params => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const authReq = axios({
+        method: 'POST',
+        url: 'http://localhost:3010/cli/check_auth',
+        data: {
+          project_id: params.pid,
+          hash: params.hash
+        }
+      })
+      .then(() => {
+        resolve('ok');
+      })
+      .catch(e => {
+        reject();
+      });
+    }, 2000);
+  });
+}
+
 const commandLine = () => {
   commander
     .usage('[options] <file>')
+    .requiredOption('--project <project>', 'use project')
+    .requiredOption('--pid <pid>', 'use project id')
+    .requiredOption('--sk <sk>', 'use secret key')
+    .requiredOption('--hash <hash>', 'use hash')
     .option('--database <database>', 'use database')
     .action(async (params, options) => {
       // console.log(params);
+
+      const spinnerReqValidity = ora('Checking request validity...').start();
+      const authReq = await checkReqValidity(params).catch(() => {})
+
+      if (!authReq) {
+        spinnerReqValidity.fail('Your cli request is invalid')
+        return;
+      }
+      spinnerReqValidity.succeed('');
       console.log('');
 
-      const databaseData = await inquirer.prompt(databaseQuestions)
-      // console.log(JSON.stringify(databaseData))
+      const databaseQuestions = [];
+      if (!params.database || !['mysql', 'postgresql', 'sqlite', 'mongodb'].includes(params.database)) {
+        databaseQuestions.push(databaseQuestion);
+      }
+
+      // If there is missing info
+      if (databaseQuestions.length) {
+        const databaseData = await inquirer.prompt(databaseQuestions)
+        if (!params.database) {
+          params.database = databaseData.database
+        }
+        // console.log(JSON.stringify(databaseData))
+      }
 
       console.log('');
 
       console.log('Installation steps:')
-      const spinner = ora('1 - Connecting to the database').start();
+      const spinner = ora('1 - Connecting to the database...').start();
 
       setTimeout(() => {
         mongodbHelper.getDatabaseSchema('localhost', 27017, '', '', 'node-express-mongodb-server', false, false, schemas => {
-          spinner.succeed();
+          spinner.succeed('1 - Connected to the database!');
 
-          const spinner2 = ora('2 - Generating project files').start();
+          const spinner2 = ora('2 - Generating project files...').start();
           setTimeout(() => {
-            spinner2.succeed();
+            spinner2.succeed('2 - Project has been generated!');
             console.log('');
             ora('Your project is ready!').succeed();
             ora('You can now start your server with the following command: "npm run dev"').info();
             console.log('');
-          }, 2000);
+          }, 3000);
 
           templateGenerator.createAdminTemplate('generated', 'mongodb', schemas);
         });
-      }, 2000);
+      }, 3000);
     })
     .parse(process.argv);
 };
